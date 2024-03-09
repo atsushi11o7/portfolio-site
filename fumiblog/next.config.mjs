d89f75a27badda295adace47bbd6713059460a20
atsushi11o7 <130956694+atsushi11o7@users.noreply.github.com>
2024-03-09 23:41:42 +0900

    update files

--- fumiblog/next.config.mjs
+++ fumiblog/next.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  
+  assetPrefix: 'https://fumiblog.com',
   trailingSlash: true,
   images: {
     loader: "custom",
     domains: ["images.microcms-assets.io"],
   },
-  
+  output: 'export',
 };
 
 export default nextConfig;
